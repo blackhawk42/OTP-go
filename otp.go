00d40ca7b9308708daca3e44ae423e76f0f62f4c
Daniel Chávez <danielsaul96@gmail.com>
2017-10-04 10:58:00 -0500

    Mayor refactoring of encryption and error handling

--- otp.go
+++ otp.go
@@ -34,77 +34,115 @@ func main() {
 	key_buffer := make([]byte, *chunk_size)
 	
 	if *key_infile == "" { // Encryption mode, or useless decryption input, somehow
-		
-		rng := bufio.NewReader(rand.Reader)
-		
-		// Create file objects
-		// Plaintext
-		fplain, err := os.Open(flag.Arg(0))
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "unexpected error while opening plaintext file\n")
-			panic(err)
-		}
-		defer fplain.Close()
-		plainReader := bufio.NewReader(fplain)
-		
-		// Ciphertext
-		fcipher, err := os.Create( fmt.Sprintf("%s.%s", fplain.Name(), DEFAULT_CIPHER_EXTENSION) )
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "unexpected error while creating ciphertext file\n")
-			panic(err)
-		}
-		defer fcipher.Close()
-		cipherWriter := bufio.NewWriter(fcipher)
-		
-		// Keyfile
-		var fkey *os.File
-		if *key_outfile == "" {
-			fkey, err = os.Create( fmt.Sprintf("%s.%s", fplain.Name(), DEFAULT_KEY_EXTENSION) )
+		var plain_filename string = flag.Arg(0)
+		var cipher_filename string = fmt.Sprintf("%s.%s", plain_filename, DEFAULT_CIPHER_EXTENSION)
+		var key_filename string
+		if *key_outfile != "" {
+			key_filename = *key_outfile
 		} else {
-			fkey, err = os.Create( *key_outfile )
+			key_filename = fmt.Sprintf("%s.%s", plain_filename, DEFAULT_KEY_EXTENSION)
 		}
+		
+		err := encryptFiles(plain_filename, key_filename, cipher_filename,
+							plain_buffer, key_buffer, cipher_buffer)
 		if err != nil {
-			fmt.Fprintf(os.Stderr, "unexpected error while creating key output file\n")
-			panic(err)
-		}
-		defer fkey.Close()
-		keyWriter := bufio.NewWriter(fkey)
-		
-		// "Crypto"
-		
-		for { // Main loop
-			n, err := plainReader.Read(plain_buffer)
-			//fmt.Printf("n: %d\n", n) // Debugging
-			if err != nil && err != io.EOF {
-				fmt.Fprintf(os.Stderr, "unexpected error while reading from plaintext\n")
-				panic(err)
-			}
-			if n == 0 {
-				break
-			}
-			
-			_, err = rng.Read(key_buffer[:n])
-			if err != nil && err != io.EOF {
-				fmt.Fprintf(os.Stderr, "unexpected error while generating key\n")
-				panic(err)
-			}
-			
-			XorSlices(plain_buffer[:n], key_buffer[:n], cipher_buffer[:n])
-			
-			_, err = cipherWriter.Write(cipher_buffer[:n])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "unexpected error while writing ciphertext\n")
-				panic(err)
-			}
-			
-			_, err = keyWriter.Write(key_buffer[:n])
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "unexpected error while writing key to file\n")
-				panic(err)
-			}
-			
+			fmt.Fprintf(os.Stderr, "%v\n", err)
 		}
 	}
+}
+
+// Simple error report
+type ErrorReport struct {
+	Msg string
+	Err error
+}
+
+func (e *ErrorReport) Error() string {
+	return fmt.Sprintf("%s: %v", e.Msg, e.Err)
+}
+
+func encryptFiles(plain_filename, key_filename, cipher_filename string,
+					plain_buffer, key_buffer, cipher_buffer []byte) error {
+	report := &ErrorReport{Err: nil}
+	
+	rng := bufio.NewReader(rand.Reader)
+		
+	// Create file objects
+	// Plaintext
+	fplain, err := os.Open(plain_filename)
+	if err != nil {
+		report.Msg = "unexpected error while opening plaintext file"
+		report.Err = err
+		return report
+	}
+	defer fplain.Close()
+	plainReader := bufio.NewReader(fplain)
+	
+	// Ciphertext
+	fcipher, err := os.Create(cipher_filename)
+	if err != nil {
+		report.Msg = "unexpected error while creating ciphertext file"
+		report.Err = err
+		return report
+	}
+	defer fcipher.Close()
+	cipherWriter := bufio.NewWriter(fcipher)
+	
+	// Keyfile
+	fkey, err := os.Create(key_filename)
+	if err != nil {
+		report.Msg = "unexpected error while creating key output file"
+		report.Err = err
+		return report
+	}
+	defer fkey.Close()
+	keyWriter := bufio.NewWriter(fkey)
+	
+	// "Crypto"
+	
+	for { // Main loop
+		n, err := plainReader.Read(plain_buffer)
+		//fmt.Printf("n: %d\n", n) // Debugging
+		if err != nil && err != io.EOF {
+			report.Msg = "unexpected error while reading from plaintext\n"
+			report.Err = err
+			return report
+		}
+		if n == 0 {
+			break
+		}
+		
+		_, err = rng.Read(key_buffer[:n])
+		if err != nil && err != io.EOF {
+			report.Msg = "unexpected error while generating key"
+			report.Err = err
+			return report
+		}
+		
+		err = XorSlices(plain_buffer[:n], key_buffer[:n], cipher_buffer[:n])
+		if err != nil {
+			report.Msg = "unexpected error while xoring slices"
+			report.Err = err
+			return report
+		}
+		
+		_, err = cipherWriter.Write(cipher_buffer[:n])
+		if err != nil {
+			report.Msg = "unexpected error while writing ciphertext\n"
+			report.Err = err
+			return report
+		}
+		
+		_, err = keyWriter.Write(key_buffer[:n])
+		if err != nil {
+			report.Msg = "unexpected error while writing key to file"
+			report.Err = err
+			return report
+		}
+		
+	}
+	
+	return nil
 }
 
 // XorSlices xors two input slices into one output, all of the same size.
